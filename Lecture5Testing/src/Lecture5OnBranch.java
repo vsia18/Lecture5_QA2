24acfe05d41ac039580906c6211d70469373dd99
vsia18 <92460996+vsia18@users.noreply.github.com>
2021-11-29 22:19:50 +0200

    Delete Lecture5Testing directory

--- Lecture5Testing/src/Lecture5OnBranch.java
+++
@@ -1,5 +0,0 @@
-public class Lecture5OnBranch {
-    public static void main(String[] args) {
-        System.out.println("this is a created branch");
-    }
-}
--- Lecture5Testing/src/Lecture5TestSample.java
+++
@@ -1,5 +0,0 @@
-public class Lecture5TestSample {
-    public static void main(String[] args) {
-        System.out.println("Hello World - This is my first commit!");
-    }
-}
